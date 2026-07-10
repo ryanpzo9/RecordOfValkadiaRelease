da94f1117fc714e8ffa7b2f17df5cf18acf78713
James Ryan <JamesRyan2209@gmail.com>
2026-07-10 18:59:46 +0700

    123

--- YggdrasilChronicles2/www/js/plugins/JR_FixPassiveStateBattleExit.js
+++
@@ -1,6 +0,0 @@
-// Fix_PassiveStateBattleExit.js
-Yanfly.APS.Scene_Battle_terminate = Scene_Battle.prototype.terminate;
-Scene_Battle.prototype.terminate = function() {
-    Yanfly.APS.Scene_Battle_terminate.call(this);
-    $gameParty.refreshMembers();
-};
